d5c716d12a1f4fde72e0ae5bf3d20db016804f76
Haadty <gggamerdosgames@gmail.com>
2024-11-11 21:48:47 -0400

    po9uwqe9hrqwhfiqwhfiwq

--- scripts/Events.gd
+++ scripts/Events.gd
@@ -5,5 +5,3 @@ var spawn_point = Vector2(211, 213)
 signal damage(type:int, damage:int)
 
 signal room_enter(room)
-
-signal battleStart()
--- scripts/battleRoom.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-func _on_player_body_entered(body:Node2D) -> void:
-	Events.room_enter.emit(self)
-	Events.battleStart.emit()
